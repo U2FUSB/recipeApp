a46e64823111d6260ad4fd90a872581319b6b3f1
niklas7U <20099542@mail.wit.ie>
2022-04-14 19:40:46 +0100

    ADD Kdoc and Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.6.10"
     application
 }
 
@@ -14,6 +16,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,4 @@
+/**
+ * Starting point of the program
+ * @author Niklas Fehringer*/
 fun main() {}
